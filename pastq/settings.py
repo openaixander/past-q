890296541a1941fb75eb5f92ec606ac70f6c493b
openaixander <164517108+openaixander@users.noreply.github.com>
2025-01-27 09:17:29 +0100

    Update settings.py

--- pastq/settings.py
+++ pastq/settings.py
@@ -146,14 +146,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 # This production code might break development mode, so we check whether we're in DEBUG mode
-if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    
-STATIC_ROOT = 'media/files'
 
 
 STATICFILES_URL = [
